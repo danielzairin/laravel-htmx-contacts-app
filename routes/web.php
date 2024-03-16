9803a9a90329fe9b5f7b16a2cce554ca236d7b02
Daniel Zairin <danielzairink@gmail.com>
2024-03-16 17:30:44 +0800

    Add and list contacts

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,19 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', function (Request $request) {
+    $contacts = DB::table('contacts')->get();
+    return view('welcome', ['contacts' => $contacts]);
+});
+
+Route::post('/contacts', function (Request $request) {
+    DB::table('contacts')->insert([
+        'name' => $request->input('name'),
+        'email' => $request->input('email'),
+    ]);
+
+    return redirect('/');
 });
